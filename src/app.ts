eeb38b682b2bebf4b18bc1d418499fe56a172a98
ulugbek-lab <bekievbek02@gmail.com>
2026-05-26 01:24:33 +0900

    fix: modify routing system and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import router from "./router";
+import routerAdmin from "./routerAdmin"
 
 /** 1-Entrance  */
 const app = express();
@@ -17,5 +18,7 @@ app.set("view engine", "ejs");
 
 /** 4-Routers  */
 
-app.use("/", router) // Middleware Design Pattern
+app.use("/admin", routerAdmin)// SSR: EJS 
+app.use("/", router)          //  SPA: REACT
+
 export default app;
--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,6 @@
-import express  from "express";
+import express from "express";
 const router = express.Router();
 import memberController from "./controllers/member.controller";
 
 
-router.get("/", memberController.goHome);
-
-router.get("/login", memberController.getLogin);
-
-router.get("/signup", memberController.getSignup);
-
-export default router
+export default router;
