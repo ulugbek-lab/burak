4ba109029cef77a9ca25b3b55a729dfc097a37c4
ulugbek-lab <bekievbek02@gmail.com>
2026-06-23 15:00:35 +0900

    fix: users page render real data

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -114,6 +114,7 @@ restaurantController.getUsers = async (req: Request, res: Response) => {
   try {
     console.log("getUsers");
     const result = await memberService.getUsers();
+    console.log(result);
     res.render("users", { users: result });
     console.log(result);
   } catch (err) {
