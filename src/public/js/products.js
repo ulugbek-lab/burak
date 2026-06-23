ef7be76c3cb0634a0bac5ae5a17ecff427d8ce85
ulugbek-lab <bekievbek02@gmail.com>
2026-06-23 16:12:31 +0900

    fix: admin project refactoring

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -23,10 +23,9 @@ $(function () {
   });
 
   $(".new-product-status").on("change", async function (e) {
-    const id = e.target.id;
-    const productStatus = $(`#${id}.new-product-status`).val();
-    console.log("id:", id);
-    console.log("productStatus", productStatus);
+    const id = e.target.id,
+      productStatus = $(`#${id}.new-product-status`).val();
+
     try {
       const response = await axios.post(`/admin/product/${id}`, {
         productStatus: productStatus,
@@ -34,7 +33,6 @@ $(function () {
       console.log("response", response);
       const result = response.data;
       if (result.data) {
-        console.log("product updated");
         $(".new-product-status").blur();
       } else alert("product update failed");
     } catch (err) {
--- src/public/js/users.js
+++ src/public/js/users.js
@@ -2,11 +2,8 @@ console.log("Users frontend javascript file");
 
 $(function () {
   $(".member-status").on("change", function (e) {
-    const id = e.target.id;
-    console.log("id", id);
-
-    const memberStatus = $(`#${id}.member-status`).val();
-    console.log("memberStatus", memberStatus);
+    const id = e.target.id,
+      memberStatus = $(`#${id}.member-status`).val();
 
     // TODO: AXIOS updateChosenUser
     axios
@@ -17,9 +14,8 @@ $(function () {
       .then((response) => {
         console.log(response);
         const result = response.data;
-        console.log(result);
+
         if (result.data) {
-          console.log("user updated");
           $(".member-status").blur();
         } else alert("user update is failed");
       })
