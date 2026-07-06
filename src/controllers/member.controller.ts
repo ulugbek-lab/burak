b55832234dd11e26f18456a6db317ec7ca0aac6e
ulugbek-lab <bekievbek02@gmail.com>
2026-07-06 18:38:47 +0900

    feat: develop updateMember business logic

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -6,6 +6,7 @@ import {
   MemberInput,
   LoginInput,
   ExtendedRequest,
+  MemberUpdateInput,
 } from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import AuthService from "../models/Auth.service";
@@ -32,7 +33,6 @@ memberController.signup = async (req: Request, res: Response) => {
     console.log("ERROR, signup:", err);
     if (err instanceof Errors) res.status(err.code).json(err);
     else res.status(Errors.standard.code).json(Errors.standard);
-    // res.json({});
   }
 };
 
@@ -78,6 +78,20 @@ memberController.getMemberDetail = async (
     res.status(HttpCode.OK).json(result);
   } catch (err) {
     console.log("ERROR, getMemberDetail:", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+  }
+};
+
+memberController.updateMember = async (req: ExtendedRequest, res: Response) => {
+  try {
+    console.log("updateMember");
+    const input: MemberUpdateInput = req.body;
+    if (req.file) input.memberImage = req.file.path; //.replace(/\\/, "/") for windows
+    const result = await memberService.updateMember(req.member, input);//argument
+    res.status(HttpCode.OK).json(result);
+  } catch (err) {
+    console.log("ERROR, updateMember:", err);
     if (err instanceof Errors) res.status(err.code).json(err);
     else res.status(Errors.standard.code).json(Errors.standard);
   }
--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -68,8 +68,20 @@ class MemberService {
     const result = await this.memberModel
       .findOne({ _id: memberId, memberStatus: MemberStatus.ACTIVE })
       .exec();
-      if(!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND)
-        return result
+    if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+    return result;
+  }
+
+  public async updateMember(
+    member: Member,
+    input: MemberUpdateInput,
+  ): Promise<Member> {
+    const memberId = shapeIntoMongooseObjectId(member._id);//parametr
+    const result = await this.memberModel
+      .findOneAndUpdate({ _id: memberId }, input, { new: true })//argument
+      .exec();
+    if (!result) throw new Errors(HttpCode.NOT_MODIFIED, Message.UPDATE_FAILED);
+    return result;
   }
 
   /** SSR */
